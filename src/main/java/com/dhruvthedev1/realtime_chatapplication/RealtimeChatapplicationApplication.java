9d09040bfb6d9e700d0483b8517e1ede44b7631b
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-10-31 16:14:46 +0000

    Initial Commit - WebSocketConfig

---
+++ src/main/java/com/dhruvthedev1/realtime_chatapplication/RealtimeChatapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.realtime_chatapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeChatapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhruvthedev1/realtime_chatapplication/RealtimeChatapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhruvthedev1.realtime_chatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeChatapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
