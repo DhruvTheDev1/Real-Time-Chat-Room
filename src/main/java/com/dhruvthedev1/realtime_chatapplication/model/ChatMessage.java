baf42f12b5612f47eace6c9265d493e4bda17e22
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-10-31 16:55:39 +0000

    Add EventListener for when use disconnects

---
+++ src/main/java/com/dhruvthedev1/realtime_chatapplication/model/ChatMessage.java
@@ -0,0 +1,22 @@
+package com.dhruvthedev1.realtime_chatapplication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class ChatMessage {
+  private String sender;
+  private MessageType messageType;
+
+  public enum MessageType {
+        CHAT, LEAVE, JOIN
+    }
+}
+
+
