d970a6db68f37b42309d71ddea6a4e12c082d9a9
DhruvTheDev1 <dhruv2015@hotmail.co.uk>
2025-11-04 14:44:15 +0000

    Add EventListener for when a user joins a chat - SessionConnectEvent

--- src/main/java/com/dhruvthedev1/realtime_chatapplication/configuration/WebSocketEventListener.java
+++ src/main/java/com/dhruvthedev1/realtime_chatapplication/configuration/WebSocketEventListener.java
@@ -4,6 +4,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import com.dhruvthedev1.realtime_chatapplication.model.ChatMessage;
@@ -18,6 +19,22 @@ import lombok.extern.slf4j.Slf4j;
 public class WebSocketEventListener {
   private final SimpMessageSendingOperations messagingTemplate;
 
+  // when a user joins
+  @EventListener
+  public void handleWebSocketConnectListener(SessionConnectEvent event) {
+    StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+    String username = (String) headerAccessor.getSessionAttributes().get("username");
+    if (username != null) {
+      log.info("User connected: {}", username);
+      ChatMessage chatMessage = ChatMessage.builder()
+          .messageType(ChatMessage.MessageType.JOIN)
+          .sender(username)
+          .build();
+      messagingTemplate.convertAndSend("/topic/public", chatMessage);
+    }
+  }
+
+  // when a user disconnects
   @EventListener
   public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
     StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
